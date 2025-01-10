bb99d2e5b612e764ced159eb6b862e94b08e8c41
mohammed Abdelaziz <m.abdelaziz1994@gmail.com>
2025-01-10 16:20:10 +0100

    feat(frontend): Joy ui init

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
-import './App.css';
+import { CssVarsProvider, extendTheme } from '@mui/joy/styles';
+
 import ProfilePage from './pages/profile';
+import './App.css';
 
 function App() {
+  const theme = extendTheme({
+    fontFamily: {
+      body: 'Source Sans Pro, sans-serif',
+      display: 'Source Sans Pro, sans-serif',
+    },
+    components: {
+      JoyButton: {
+        styleOverrides: {
+          root: {
+            backgroundColor: '#153376',
+            color: '#ffffff',
+            '&:hover': {
+              backgroundColor: '#153376',
+            },
+          },
+        },
+      },
+    },
+  });
+
   return (
-    <div className="root">
+    <CssVarsProvider theme={theme}>
       <ProfilePage />
-    </div>
+    </CssVarsProvider>
   );
 }
 
